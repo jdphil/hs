dfedc8792698a41160fc9ea905881e455086b2f7
jdphil <jdphil@gmail.com>
2016-08-30 19:29:14 +0100

    Changed the HTML

--- habitstation/urls.py
+++ habitstation/urls.py
@@ -19,4 +19,6 @@ from habitstation import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+    url(r'^about/', views.about, name='about'),
+    url(r'^contact/', views.contact, name='contact'),
 ]
--- main/urls.py
+++ main/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', include('habitstation.urls')),
+    url(r'^', include('habitstation.urls')),
 ]
