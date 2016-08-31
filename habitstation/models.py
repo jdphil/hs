ba1257e5e81ed7d9ccf87ff2f47f0a5e64a10a49
Jonathan Phillipo <jonathanp@local.pensionsmanagement.co.uk>
2016-08-31 16:41:42 +0100

    change to html and start on models

--- habitstation/models.py
+++ habitstation/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Habit(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    created_date = models.DateTimeField(
+            default=timezone.now)
+
+    def __str__(self):
+        return self.name
+
+class User_Habit(models.Model):
+	user = models.ForeignKey('auth.User')
+	habit = models.ForeignKey(Habit, on_delete=models.CASCADE)
+	created_date = models.DateTimeField(
+            default=timezone.now)
+
+	def __str__(self):
+		return '%s %s' % (self.habit, self.user)
+
+		
